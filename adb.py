58a38ed6411998942da5a239f97c66f25e4e10ac
ed9ar <a01339939@itesm.mx>
2020-05-30 12:55:12 -0500

    Menu
    Menu

--- adb.py
+++ adb.py
@@ -1,5 +1,4 @@
 
-# Pelis por actor
 from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
@@ -27,7 +26,8 @@ def movieInfoMongo(title):
     #pensar parametros
     print(title)
     for x in mycol.find({"title": str(title)},{  "_id": 0, "title": 1,"description":1, "duration": 1, "type":1 , "rating": 1, "listed_in":1 }):
-        print(x)
+        pprint(x)
+        print()
     #escribir los resultados a redis
 
 def actorInfoRedis():
@@ -129,5 +129,49 @@ def generoInfoMongo(genero):
 
 
 #Aqui va el menu
+ans = ""
+while ans != "N":
+    print("Welcome to Netflix titles app")
+    print()
+    print("What would you like to know today?")
+    print("1. Movie info by title")
+    print("2. Actor info by name")
+    print("3. Director info by name")
+    print("4. Titles by year")
+    print("5. Titles by country")
+    print("6. Titles by rating")
+    print("7. Titles by genre")
+    print("")
+
+    option = input("Write the number of your choice: ")
+    if int(option) == 1:
+        movie = input("Write the title of the movie: ")
+        #movieInfoRedis(movie)
+        movieInfoMongo(movie)
+    elif int(option) == 2:
+        actor = input("Write the name of the actor: ")
+    elif int(option) == 3:
+        director = input("Write the name of the director: ")
+    elif int(option) == 4:
+        year = input("Write the year: ")
+    elif int(option) == 5:
+        country = input("Write the country: ")
+    elif int(option) == 6:
+        print("Highest or Lowest ?")
+        print("Highest = H")
+        print("Lowest = L")
+        horl = input("")
+    elif int(option) == 7:
+        genre = input("Write the genre: ")
+    else:
+        print("Please choose a valid option")
+    
+    print()
+    print("Would you like to continue?")
+    print("Yes = Y")
+    print("No = N")
+    print()
+    ans = input("")
+    
 
 
